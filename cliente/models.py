2cc91458154032628509c7dde5db0d0cd9cbfcdb
Anaeli Neira <anaeli.neirav@gmail.com>
2023-06-21 21:56:51 -0400

    Actualizacion Anaeli

--- cliente/models.py
+++ cliente/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-cliente=get_user_model()
+User=get_user_model()
 
 # Create your models here.
 class Estado(models.Model):
@@ -15,7 +15,7 @@ class Obras(models.Model):
     Obras = models.CharField(max_length=255)
     descripcion = models.TextField(default="")
     imagen = models.ImageField(upload_to='img/')
-    idUsuario = models.ForeignKey(cliente,on_delete=models.CASCADE)
+    idUsuario = models.ForeignKey(User,on_delete=models.CASCADE)
     estado = models.ForeignKey('Estado', on_delete=models.CASCADE,default=1)
 
     def __str__(self):
--- cliente/views.py
+++ cliente/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
+from django.contrib.auth.models import User
+from .forms import clienteFormulario,ObrasFormulario
+from django.contrib.auth.decorators import login_required
+from .models import Obras,Estado
 
 # Create your views here.
 
 def main(request):
     context={}
+    if request.user.is_autenticated :
+        context["username"] = request.user.username
     return render(request, 'cliente/main.html', context)
 
 def login(request):
@@ -33,3 +41,11 @@ def vicentvangogh(request):
 def artista(request):
     context={}
     return render(request, 'cliente/artista.html', context)
+
+def obras(request):
+    context={}
+    return render(request, 'cliente/obras.html', context)
+
+def base(request):
+    context={}
+    return render(request, 'cliente/base.html', context)
